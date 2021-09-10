85150f7336f7ffb2797e9683e98b28d823bf240e
jose henrique holanda cavalcanti <josehenrique.holanda1@gmail.com>
2021-09-10 10:33:31 -0400

    home com filter e render da lista de produtos do restaurante

--- src/pages/RestaurantPage/RestaurantPage.js
+++ src/pages/RestaurantPage/RestaurantPage.js
@@ -1,11 +1,47 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
+import axios from 'axios'
+import { BASE_URL } from "../../constants/urls"
+import { useParams } from "react-router"
+
 
 const RestaurantPage=()=>{
+    const id = useParams()
+    const [restaurantInfo, setRestaurantInfo] = useState([])
+    const [products, setRestaurantProducts] = useState([])
+    const getRestaurantInfo = () => {
+        axios.get(`${BASE_URL}/restaurants/${id.id}`, {headers : {
+            auth: localStorage.getItem('token')
+        }}).then((response) => {
+            
+            setRestaurantInfo(response.data.restaurant)
+            setRestaurantProducts(response.data.restaurant.products)
+            
+            
+            
+        }).catch((err) => {
+            console.log(err)
+        })
+    }
+
+    useEffect(() => {
+        getRestaurantInfo()
+      
+    }, [])
+    
     return(
+        <>
         <div>
             home page
-
         </div>
+        <div>
+        {<div><p>{restaurantInfo.name}</p> <p>{restaurantInfo.description}</p> <img style={{height: 200, width: 250}} src={restaurantInfo.logoUrl}/></div>}
+        {products.map((item,index) => (
+            <div key={index}><img style={{height: 200, width: 250}} src={item.photoUrl}/> <p>{item.name}</p> <p>{item.description}</p> <p>R${item.price}</p></div>
+        )
+            
+        )}
+        </div>
+        </>
     )
 }
 export default RestaurantPage
